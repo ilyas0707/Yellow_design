5955f6b8c55c5304c8de8852d25aa00db215489c
ilyas0707 <48711796+ilyas0707@users.noreply.github.com>
2019-05-31 23:01:01 +0600

    yellow

---
+++ js/core.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('#mob-m').click(function(){		
+			$('.menu').slideToggle(700);	
+			if ($(this).hasClass('not-active')) {
+				$(this).addClass('is-active').removeClass('not-active');
+			}else{
+				setTimeout(function(){
+					$('.is-active').addClass('not-active').removeClass('is-active')
+				},600)			
+			}		
+		});
+
+});
